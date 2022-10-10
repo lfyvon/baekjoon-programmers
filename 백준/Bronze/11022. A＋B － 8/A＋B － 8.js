ef25cc47177996036abf713af3f618cf4fefe59f
lfyvon <110438191+lfyvon@users.noreply.github.com>
2022-10-10 18:25:13 +0900

    [Bronze V] Title: A+B - 8, Time: 116 ms, Memory: 9392 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,11 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const t = parseInt(input[0]);
+let result = [];
+
+for(let i = 1; i <= t; i++){
+    const [a, b] = input[i].split(' ').map(el => parseInt(el));
+    result.push("Case #"+ i +": " + a + " + " + b +" = "+ (a + b));
+}
+
+console.log(result.join('\n'));
