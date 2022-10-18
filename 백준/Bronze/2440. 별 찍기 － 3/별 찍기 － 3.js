e6fffa062bb16d7eb3c89109afe57ee5c9f84fe0
lfyvon <110438191+lfyvon@users.noreply.github.com>
2022-10-18 18:32:44 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 120 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.js
@@ -0,0 +1,8 @@
+const n = parseInt(require('fs').readFileSync('/dev/stdin').toString());
+
+let result = [];
+for(let i = n; i > 0; i--){
+    result.push('*'.repeat(i));
+}
+
+console.log(result.join('\n'));
