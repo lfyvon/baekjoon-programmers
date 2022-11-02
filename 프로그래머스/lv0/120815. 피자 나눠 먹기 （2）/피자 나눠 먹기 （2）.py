3260662637d9a19b7cff2e1c28e24259328b703a
lfyvon <110438191+lfyvon@users.noreply.github.com>
2022-11-02 16:15:06 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,7 +1,4 @@
+import math
+
 def solution(n):
-    answer = 0
-    for i in range(max(n, 6), (n * 6) + 1):
-        if i % n == 0 and i % 6 == 0:
-            answer = i // 6
-            break
-    return answer
+    return n * 6 // math.gcd(n, 6) // 6
