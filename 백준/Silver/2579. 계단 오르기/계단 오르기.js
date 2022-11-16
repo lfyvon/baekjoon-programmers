32f79e185360149d166eae977add3c8b7838d42e
lfyvon <110438191+lfyvon@users.noreply.github.com>
2022-11-16 15:01:02 +0900

    [Silver III] Title: 계단 오르기, Time: 140 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.js
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -1,16 +1,14 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(el => parseInt(el));
+const stairs = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(el => parseInt(el));
 
-const n = input[0];
-const stairs = input.slice(1);
-
+const n = stairs[0];
 const dp = new Array(n + 1);
 
-dp[1] = stairs[0];
-dp[2] = stairs[0] + stairs[1];
-dp[3] = Math.max(stairs[0], stairs[1]) + stairs[2];
+dp[1] = stairs[1];
+dp[2] = stairs[1] + stairs[2];
+dp[3] = Math.max(stairs[1], stairs[2]) + stairs[3];
 
 for(let i = 4; i <= n; i++){
-    dp[i] = Math.max(dp[i - 2], dp[i - 3] + stairs[i - 2]) + stairs[i - 1];
+    dp[i] = Math.max(dp[i - 2], dp[i - 3] + stairs[i - 1]) + stairs[i];
 }
 
 console.log(dp[n]);
