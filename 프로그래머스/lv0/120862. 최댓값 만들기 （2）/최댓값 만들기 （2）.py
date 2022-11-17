114a305d62c1b4fc49bf344434cdd3737fc58bc4
lfyvon <110438191+lfyvon@users.noreply.github.com>
2022-11-17 14:56:31 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,3 +1,3 @@
 def solution(numbers):
     numbers.sort()
-    return max(numbers[0] * numbers[1], numbers[len(numbers) - 1] * numbers[len(numbers) - 2])
+    return max(numbers[0] * numbers[1], numbers[- 1] * numbers[- 2])
