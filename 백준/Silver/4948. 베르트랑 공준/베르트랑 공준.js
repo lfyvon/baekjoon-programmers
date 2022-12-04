a51432c983dd7d8bdba3d9316e530e9dd6ceb9e5
lfyvon <110438191+lfyvon@users.noreply.github.com>
2022-12-04 09:02:19 +0900

    [Silver II] Title: 베르트랑 공준, Time: 204 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.js
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.js
@@ -2,6 +2,8 @@ const t = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(el
 const max = 123456 * 2
 const prime = new Array(max * 2 + 1).fill(true);
 prime[0] = prime[1] = false;
+const result = [];
+
 for (let i = 2; i * i<= max; i++) {
     if (prime[i]) {
         for (let j = i * 2; j <= max * 2; j += i) {
@@ -12,17 +14,16 @@ for (let i = 2; i * i<= max; i++) {
 let i = 0;
 
 while (t[i] !== 0) {
-  const n = t[i];
-
-  const results = [];
-
-  for (let i = n + 1; i <= n * 2; i++) {
-    if (prime[i]) {
-      results.push(i);
+    const n = t[i];
+    let count = 0;
+    for (let i = n + 1; i <= n * 2; i++) {
+        if (prime[i]) {
+            count++;
+        }
     }
-  }
+    result.push(count);
 
-  console.log(results.length);
-
-  i++;
+    i++;
 }
+
+console.log(result.join('\n'));
