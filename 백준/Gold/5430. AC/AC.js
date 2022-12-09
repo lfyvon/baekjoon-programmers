a853e74af7c12c4d2fc1109ae8f65d307e33dafc
lfyvon <110438191+lfyvon@users.noreply.github.com>
2022-12-09 12:20:54 +0900

    [Gold V] Title: AC, Time: 456 ms, Memory: 59160 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.js
+++ 백준/Gold/5430. AC/AC.js
@@ -6,7 +6,10 @@ let result = "";
 for(let i = 0; i < t * 3; i += 3){
     const p = AC[i].trim().split('');
     const n = parseInt(AC[i + 1]);
-    const x = AC[i + 2].slice(1, -1).split(',').map(el => parseInt(el)).filter(Boolean);
+    let x = AC[i + 2].slice(1, -1).split(',').map(el => parseInt(el)).filter(Boolean);
+    let len = x.length;
+    let left = 0;
+    let right = n;
     let isReverse = false;
     let check = true;
     for(let j = 0; j < p.length; j++){
@@ -14,13 +17,15 @@ for(let i = 0; i < t * 3; i += 3){
             isReverse = !isReverse;
         }
         if(p[j] === 'D'){
-            if(!x.length) check = false;
+            if(!len) check = false;
             else{
-                if(isReverse) x.pop();
-                else x.shift();
+                if(isReverse) right--;
+                else left++;
+                len--;
             }   
         }
     }
+    x = x.slice(left, right);
     if(isReverse) x.reverse();
     if(check) result += '[' + x.join(',') +']' + '\n';
     else result += 'error' + '\n';
