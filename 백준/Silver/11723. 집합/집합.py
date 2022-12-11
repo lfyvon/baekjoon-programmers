a32de291ca75ec4c8ba864344eed058a58803345
lfyvon <110438191+lfyvon@users.noreply.github.com>
2022-12-11 12:25:07 +0900

    [Silver V] Title: 집합, Time: 3348 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -5,20 +5,18 @@ bit = 0
 t = int(input())
 for i in range(t):
     cmd = input().split()
-    if cmd[0] == 'all':
-            bit = (1 << 20) - 1
+    if cmd[0] == 'add':
+        bit |= (1 << int(cmd[1]))
+    elif cmd[0] == 'remove':
+        bit &= ~(1 << int(cmd[1]))
+    elif cmd[0] == 'check':
+        if bit & (1 << int(cmd[1])) == 0:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'all':
+        bit = (1 << 21) - 1
     elif cmd[0] == 'empty':
         bit = 0
-    else:
-        num = int(cmd[1]) - 1
-        if cmd[0] == 'check':
-            if bit & (1 << num) == 0:
-                print(0)
-            else:
-                print(1)
-        elif cmd[0] == 'add':
-            bit |= (1 << num)
-        elif cmd[0] == 'remove':
-            bit &= ~(1 << num)
-        elif cmd[0] == 'toggle':
-            bit = bit ^ (1 << num)
+    elif cmd[0] == 'toggle':
+        bit ^= (1 << int(cmd[1]))
