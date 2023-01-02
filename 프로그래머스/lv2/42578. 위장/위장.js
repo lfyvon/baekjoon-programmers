b313b93410133a7307db7d37fbaab18a9f1c5853
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-01-02 17:17:20 +0900

    [level 2] Title: 위장, Time: 4.82 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -0,0 +1,17 @@
+function solution(clothes) {
+    var answer = 0;
+    const costume = new Map();
+    for(let i = 0; i < clothes.length; i++){
+        const [name, type] = clothes[i];
+        if(costume.has(type)){
+            costume.set(type, costume.get(type) + 1)
+        } else{
+            costume.set(type, 1)
+        }
+        console.log(costume)
+    }
+    let count = [...costume.values()].reduce((a, b) => a * (b + 1), 1) - 1;
+    if(count < 0) count = 0;
+    answer = count;
+    return answer;
+}
