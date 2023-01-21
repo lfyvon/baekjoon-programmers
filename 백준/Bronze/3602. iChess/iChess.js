f839f7d68948a6a657e0efe59582623852a2003f
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-01-21 19:18:37 +0900

    [Bronze II] Title: iChess, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/3602. iChess/iChess.js
+++ 백준/Bronze/3602. iChess/iChess.js
@@ -1,5 +1,4 @@
 const [B, W] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
-let result  = 0;
 let [min, max] = [Math.min(B, W), Math.max(W, B)];
-result = Math.floor(Math.sqrt(min === max ? min + max : min * 2 + 1, 2));
+let result = Math.floor(Math.sqrt(min === max ? min + max : min * 2 + 1, 2));
 console.log(!result ? 'Impossible' : result);
