fa87ad3db8a58fd6e89662795b9d89d6a1253db4
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-02-18 12:22:09 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.js
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.js
@@ -1,3 +1,3 @@
 const n = Number(require('fs').readFileSync('/dev/stdin').toString());
-const result = n % 8;
-console.log(result === 1 ? 1 : result === 2 || result === 0 ? 2 : result === 3 || result === 7 ? 3 : result === 4 || result === 6 ? 4 : 5);
+const r = n % 8;
+console.log(r === 1 ? 1 : r === 2 || r === 0 ? 2 : r === 3 || r === 7 ? 3 : r === 4 || r === 6 ? 4 : 5);
