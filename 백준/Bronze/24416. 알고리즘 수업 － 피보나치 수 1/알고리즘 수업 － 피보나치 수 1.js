db592ca4ff8bffff251acea9e0dcb4239c1af356
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-03-01 19:22:56 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 164 ms, Memory: 9576 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.js
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.js
@@ -1,13 +1,10 @@
 const N = Number(require('fs').readFileSync('/dev/stdin').toString());
 const f = new Array(N + 1).fill(0);
 f[1] = f[2] = 1;
-
 const fib = (n) => {
-    if(n === 1 || n === 2){
-        return 1;
-    } else{
-        return (fib(n - 1) + fib(n - 2));
+    for(let i = 3; i <= n; i++){
+        f[i] = f[i - 1] + f[i -2];
     }
+    return f[n];
 }
-
 console.log(fib(N) + " " + (N - 2));
