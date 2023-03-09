5a6eb5f763de5525328dd27b9a5ff54994b99294
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-03-09 10:36:26 +0900

    [Bronze V] Title: 사칙연산, Time: 152 ms, Memory: 9732 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,17 @@
+var readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+rl.on("line", function(line) {
+    input = line.split(' ').map((el) => parseInt(el));
+}).on("close", function() {
+    console.log(input[0] + input[1]);
+    console.log(input[0] - input[1]);
+    console.log(input[0] * input[1]);
+    console.log(Math.trunc(input[0] / input[1]));
+    console.log(input[0] % input[1]);
+    process.exit();
+});
