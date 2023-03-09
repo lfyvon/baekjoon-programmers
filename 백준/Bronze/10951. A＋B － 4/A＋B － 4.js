b8eb262a1ff32d95535826272643340b10574a31
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-03-09 10:41:12 +0900

    [Bronze V] Title: A+B - 4, Time: 140 ms, Memory: 9516 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,16 @@
+var readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+let result = [];
+rl.on("line", function(line){
+    input.push(line.split(' ').map(Number));
+}).on("close", function() {
+    for(let i =0; i < input.length; i++){
+        result.push(input[i][0]+input[i][1]);
+    }
+    console.log(result.join('\n'));
+})
