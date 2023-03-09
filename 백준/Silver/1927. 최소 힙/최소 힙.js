a39562cfe6c2d5e2d5b2a207b58ec8a195637a0f
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-03-09 11:54:45 +0900

    [Silver II] Title: 최소 힙, Time: 280 ms, Memory: 24284 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.js
@@ -0,0 +1,76 @@
+class MinHeap {
+    constructor() {
+        this.heap = [ null ];
+    }
+    
+    size() {
+        return this.heap.length - 1;
+    }
+    
+    getMin() {
+        return this.heap[1] ? this.heap[1] : null;
+    }
+    
+    swap(a, b) {
+        [ this.heap[a], this.heap[b] ] = [ this.heap[b], this.heap[a] ];
+    }
+    
+    insert(value) {
+        this.heap.push(value);
+        let curIdx = this.heap.length - 1;
+        let parIdx = (curIdx / 2) >> 0;
+        
+        while(curIdx > 1 && this.heap[parIdx] > this.heap[curIdx]) {
+            this.swap(parIdx, curIdx)
+            curIdx = parIdx;
+            parIdx = (curIdx / 2) >> 0;
+        }
+    }
+    
+    delete() {
+        const min = this.heap[1];	
+        if(this.heap.length <= 2) this.heap = [ null ];
+        else this.heap[1] = this.heap.pop();   
+        
+        let curIdx = 1;
+        let leftIdx = curIdx * 2;
+        let rightIdx = curIdx * 2 + 1; 
+        
+        if(!this.heap[leftIdx]) return min;
+        if(!this.heap[rightIdx]) {
+            if(this.heap[leftIdx] < this.heap[curIdx]) {
+                this.swap(leftIdx, curIdx);
+            }
+            return min;
+        }
+
+        while(this.heap[leftIdx] < this.heap[curIdx] || this.heap[rightIdx] < this.heap[curIdx]) {
+            const minIdx = this.heap[leftIdx] > this.heap[rightIdx] ? rightIdx : leftIdx;
+            this.swap(minIdx, curIdx);
+            curIdx = minIdx;
+            leftIdx = curIdx * 2;
+            rightIdx = curIdx * 2 + 1;
+        }
+
+        return min;
+    }
+}
+
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(el => parseInt(el));
+
+let num = input[0];
+let minQueue = new MinHeap();
+let result = [];
+for(let i = 1; i <= num; i++){
+    if(input[i] === 0){
+        if(minQueue.size() === 0){
+            result.push(0);
+        } else {
+            result.push(minQueue.getMin());
+            minQueue.delete();
+        }
+    } else {        
+        minQueue.insert(input[i]);
+    }
+}
+console.log(result.join('\n').trim());
