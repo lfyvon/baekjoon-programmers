1f4e1dec4881dd7c9af296c140a1b9f2508731e0
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-03-15 15:29:57 +0900

    [Silver IV] Title: 박 터뜨리기, Time: 136 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.js
@@ -0,0 +1,8 @@
+const [n, k] = require('fs').readFileSync('/dev/stdin').toString().split(' ').map(Number);
+const sum = k * (k + 1) / 2;
+let result = 0;
+const r = (n - sum) % k;
+if(sum > n) result = -1;
+else if((n - sum) % k === 0) result = k - 1;
+else result = k;
+console.log(result);
