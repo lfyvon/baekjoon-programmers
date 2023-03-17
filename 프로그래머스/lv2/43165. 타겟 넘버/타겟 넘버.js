77ec152eeea63313e96cd5fefbfe2afa06a2fc47
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-03-17 12:16:18 +0900

    [level 2] Title: 타겟 넘버, Time: 1.20 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -5,7 +5,6 @@ function solution(numbers, target) {
             if(sum === target){
                 answer++;
             }
-            return;
         } else{
             dfs(i + 1, sum + numbers[i]);
             dfs(i + 1, sum - numbers[i]);
