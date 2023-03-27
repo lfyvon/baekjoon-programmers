e5af7ee40ef5426794cb541dbdc25afa781e228c
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-03-27 15:20:19 +0900

    [Bronze III] Title: Right Triangle, Time: 120 ms, Memory: 9460 KB -BaekjoonHub

--- 백준/Bronze/9723. Right Triangle/Right Triangle.js
+++ 백준/Bronze/9723. Right Triangle/Right Triangle.js
@@ -1,9 +1,9 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 const t = Number(input[0]);
-const result = [];
+let result = "";
 for(let i = 1; i <= t; i++){
     const [a, b, c] = input[i].split(' ').map(Number).sort((a, b) => a - b);
-    let str = `Case #${i}: `;
-    result.push(c ** 2 === a ** 2 + b ** 2 ? str += 'YES' : str += 'NO');
+    result += `Case #${i}: `;
+    c ** 2 === a ** 2 + b ** 2 ? result += 'YES\n' : result += 'NO\n';
 }
-console.log(result.join('\n'));
+console.log(result.trim());
