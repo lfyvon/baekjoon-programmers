d40521105f6ba7a2e6103eda7fd14e755cde0a9f
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-04-02 21:39:04 +0900

    [Silver II] Title: 섬의 개수, Time: 180 ms, Memory: 10404 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.js
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.js
@@ -1,36 +1,19 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const dx = [-1, 0, 1, -1, 1, -1, 0, 1];
 const dy = [-1, -1, -1, 0, 0, 1, 1, 1];
-let w, h, graph, visited;
+let w, h, graph;
 const result = [];
 let i = 0;
 
-const countLand = () => {
-    let count = 0;
-    for(let i = 0; i < h; i++){
-      for(let j = 0; j < w; j++){
-        if(graph[i][j]){
-          BFS(i, j);
-          count++;
+const DFS = (x, y) => {
+    if(x < 0 || y < 0 || x >= h || y >= w) return;
+    if(graph[x][y]){
+        graph[x][y] = 0;
+        for(let i = 0; i < 8; i++){
+            const xPos = x + dx[i];
+            const yPos = y + dy[i];
+            DFS(xPos, yPos);
         }
-      }
-    }
-    result.push(count);
-}
-
-const BFS = (startX, startY) => {
-    const queue = [[startX, startY]];
-    while(queue.length){
-      const [x, y] = queue.shift();
-      if(!graph[x][y]) continue;
-      else graph[x][y] = 0;
-      
-      for(let i = 0; i < 8; i++){
-        const xPos = x + dx[i];
-        const yPos = y + dy[i];
-        if(xPos < 0 || yPos < 0 || xPos >= h || yPos >= w) continue;
-        if(graph[xPos][yPos]) queue.push([xPos, yPos]);
-      }
     }
 }
 
@@ -39,7 +22,16 @@ while(true){
     [w, h] = input[i].split(' ').map(Number);
     i++;
     graph = input.slice(i, i + h).map(el => el.split(' ').map(Number));
-    countLand();
+    let count = 0;
+    for(let i = 0; i < h; i++){
+      for(let j = 0; j < w; j++){
+        if(graph[i][j]){
+          DFS(i, j);
+          count++;
+        }
+      }
+    }
+    result.push(count);
     i += h;
 }
 
