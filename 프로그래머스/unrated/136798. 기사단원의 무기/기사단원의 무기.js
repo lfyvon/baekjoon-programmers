5772cb7160618a42e3d675c3bd2fcbd1d0b26698
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-04-04 15:17:20 +0900

    [unrated] Title: 기사단원의 무기, Time: 2.55 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,16 @@
+function solution(number, limit, power) {
+    var answer = 0;
+    const arr = [1];
+    for(let i = 2; i <= number; i++){
+        let count = 0;
+        for(let j = 1; j <= Math.sqrt(i); j++){
+            if(i % j === 0) count++;
+        }
+        count *= 2;
+        if(Number.isInteger(Math.sqrt(i))) count--;
+        if(count > limit) count = power;
+        arr.push(count);
+    }
+    answer = arr.reduce((a, b) => a + b);
+    return answer;
+}
