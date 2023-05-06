e2b594c1e86fdf3f417244e0d7dffdf5d6d4ff30
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-05-06 17:21:17 +0900

    [Bronze II] Title: 점수 집계, Time: 128 ms, Memory: 9316 KB -BaekjoonHub

--- 백준/Bronze/9076. 점수 집계/점수 집계.js
+++ 백준/Bronze/9076. 점수 집계/점수 집계.js
@@ -2,8 +2,8 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('
 const t = Number(input[0]);
 const result = [];
 for(let i = 1; i <= t; i++){
-    const score = input[i].split(' ').map(Number).sort((a, b) => a - b).slice(1, 4);
-    const total = score[2] - score[0] >= 4 ? "KIN" : score.reduce((a, b) => a + b);
+    const score = input[i].split(' ').map(Number).sort((a, b) => a - b);
+    const total = score[3] - score[1] >= 4 ? "KIN" : score[1] + score[2] + score[3];
     result.push(total);
 }
 console.log(result.join('\n'));
