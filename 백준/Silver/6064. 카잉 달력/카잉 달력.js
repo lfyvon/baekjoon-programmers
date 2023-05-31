60cf4ad7458751df5db242bbc3e44a0645355310
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-05-31 17:39:38 +0900

    [Silver I] Title: 카잉 달력, Time: 232 ms, Memory: 10992 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.js
@@ -0,0 +1,28 @@
+const [T, ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const data = input.map(v => v.split(' ').map(Number));
+
+const getGCD = (a, b) => {
+  let x = Math.max(a, b);
+  let y = Math.min(a, b);
+  let re;
+  while (y) {
+    re = x % y;
+    x = y;
+    y = re;
+  }
+  return x;
+};
+
+const output = data.map(([M, N, x, y]) => {
+  const lcm = M * N / getGCD(M, N);
+  let year = -1;
+  for (let i = x; i <= lcm; i += M) {
+    if ((i - 1) % N + 1 === y) {
+      year = i;
+      break;
+    }
+  }
+  return year;
+});
+
+console.log(output.join('\n'));
