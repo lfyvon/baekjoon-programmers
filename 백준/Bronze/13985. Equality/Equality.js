85db77f3884954f34fd4dab3100cd6617bb07901
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-07-16 12:34:25 +0900

    [Bronze IV] Title: Equality, Time: 140 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/13985. Equality/Equality.js
+++ 백준/Bronze/13985. Equality/Equality.js
@@ -1,2 +1,2 @@
-const [a, op, b, _, sum] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+const [a, , b, , sum] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
 console.log(a + b === sum ? "YES" : "NO");
