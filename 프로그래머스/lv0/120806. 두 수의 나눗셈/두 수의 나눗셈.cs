cfb1d5a5c6c88540dd550aba174aa5571ccf682e
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-07-23 00:54:40 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.13 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Solution {
     public int solution(int num1, int num2) {
-        int answer = num1 * 1000 / num2;
+        int answer = (int)((float)num1 / (float)num2 * 1000);
         return answer;
     }
 }
