9b23cfa42701eef3be2a3abe1e8d8c0af53d068a
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-08-10 17:00:54 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 304 ms, Memory: 65276 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.js
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.js
@@ -1,9 +1,9 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const [n, m] = input[0].split(" ").map(Number);
-const campus = input.slice(1).map(el => el.trim().split(""));
+const campus = input.slice(1).map(el => el.trim());
 const dx = [1, 0, -1, 0];
 const dy = [0, 1, 0, -1];
-const visited = Array.from(new Array(n), () => Array(m).fill(0));
+const visited = Array.from(new Array(n), () => Array(m).fill(false));
 let [ix, iy] = [0, 0];
 let count = 0;
 
@@ -14,14 +14,16 @@ for(let i = 0; i < n; i++){
 }
 
 const dfs = (x, y) => {
-    visited[x][y] = 1;
+    visited[x][y] = true;
+    if(campus[x][y] === "P") count++;
     for(let i = 0; i < 4; i++){
         let posX = x + dx[i];
         let posY = y + dy[i];
 
-        if(posX < 0 || posX >= n || posY < 0 || posY >= m || campus[posX][posY] === "X" || visited[posX][posY]) continue;
-        if(campus[posX][posY] === "P") count++;
-        dfs(posX, posY);
+        if(posX < 0 || posX >= n || posY < 0 || posY >= m) continue;
+        if(campus[posX][posY] !== "X" && !visited[posX][posY]){
+            dfs(posX, posY);
+        }
     }
 }
 dfs(ix, iy);
