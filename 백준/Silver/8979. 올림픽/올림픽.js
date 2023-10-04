c494a61a3e206744367aa2d3680e425be9e42ecd
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-10-04 18:39:48 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 9588 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.js
+++ 백준/Silver/8979. 올림픽/올림픽.js
@@ -1,24 +1,21 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(el => el.split(" ").map(Number));
 const [N, K] = input[0];
+const nation = input.slice(1);
 let st = "";
 let count = 1;
-const nation = input.slice(1).sort((a, b) => {
-    if(a[1] === b[1]){
-        if(a[2] === b[2]){
-            return b[3] - a[3];
-        }
-        return b[2] - b[2];
-    }
-    return b[1] - a[1]; 
-});
 
 for(let i = 0; i < N; i++){
     if(nation[i][0] === K) st = nation[i];
 }
 
 for(let i = 0; i < N; i++){
-    if(st[1] === nation[i][1] && st[2] === nation[i][2] && st[3] === nation[i][3]) break;
-    count++;
+    if(nation[i][1] > st[1]){
+        count++;
+    } else if(nation[i][1] === st[1] && nation[i][2] > st[2]){
+        count++;
+    } else if(nation[i][1] === st[1] && nation[i][2] === st[2] && nation[i][3] > st[3]){
+        count++;  
+    }
 }
 
 console.log(count);
