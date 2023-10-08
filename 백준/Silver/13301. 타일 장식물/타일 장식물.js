e805b465418a1ec3c187b1e0df08b5f244520b57
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-10-09 07:55:04 +0900

    [Silver V] Title: 타일 장식물, Time: 128 ms, Memory: 9344 KB, Score: 74 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.js
@@ -0,0 +1,8 @@
+const n = Number(require('fs').readFileSync('/dev/stdin').toString());
+const dp = new Array(n + 1).fill(0);
+dp[1] = 4;
+dp[2] = 6;
+for(let i = 3; i <= n; i++){
+    dp[i] = dp[i - 1] + dp[i - 2];
+}
+console.log(dp[n]);
