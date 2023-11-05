412e2cfa9993b1e7ebc0879367aee69292d7ba71
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-11-05 18:36:44 +0900

    [Silver IV] Title: 국영수, Time: 488 ms, Memory: 57588 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.js
+++ 백준/Silver/10825. 국영수/국영수.js
@@ -1,17 +1,9 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const n = Number(input[0]);
 const list = input.slice(1).map(el => el.trim().split(" ")).sort((a, b) => {
-    if(+a[1] === +b[1]){
-        if(+a[2] === +b[2]){
-            if(+a[3] === +b[3]){
-                if (a[0] > b[0]) return 1;
-                else if (a[0] < b[0]) return -1;
-                else return 0;
-            }
-            return +b[3] - +a[3];
-        }
-        return +a[2] - +b[2];
-    }
-    return +b[1] - +a[1];
+    if(+a[1] !== +b[1]) return +b[1] - +a[1];
+    if(+a[2] !== +b[2]) return +a[2] - +b[2];
+    if(+a[3] !== +b[3]) return +b[3] - +a[3];
+    return a[0] > b[0] ? 1 : a[0] < b[0] ? -1 : 0;
 }).map(el => el[0]);
 console.log(list.join("\n"));
