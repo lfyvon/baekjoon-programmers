8957dbb39559d2231c866f1a37c6696e46340e29
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-11-16 21:39:46 +0900

    [Silver V] Title: 반지, Time: 116 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/5555. 반지/반지.js
+++ 백준/Silver/5555. 반지/반지.js
@@ -4,6 +4,6 @@ const n = Number(input[1]);
 let result = 0;
 for(let i = 2; i < n + 2; i++){
     const ring = input[i] + input[i];
-    if(ring.split(str).length - 1) result++;
+    if(ring.includes(str)) result++;
 }
 console.log(result);
