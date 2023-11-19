9b1e669cdd6306b9497ab3f4bd774c3042b54de5
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-11-20 06:58:53 +0900

    [Bronze V] Title: Lucky 7, Time: 124 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/30224. Lucky 7/Lucky 7.js
+++ 백준/Bronze/30224. Lucky 7/Lucky 7.js
@@ -1,2 +1,6 @@
 const n = require('fs').readFileSync('/dev/stdin').toString().trim();
-console.log(n.includes('7') ? +n % 7 === 0 ? 3 : 2 : +n % 7 === 0 ? 1 : 0);
+if(n.includes('7')){
+    console.log(+n % 7 === 0 ? 3 : 2);
+} else{
+    console.log(+n % 7 === 0 ? 1 : 0);
+}
