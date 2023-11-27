b2f9ee27f89e8a85b5239d28c01502ef6b8e9596
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-11-27 23:12:17 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 588 ms, Memory: 133180 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.js
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.js
@@ -1,30 +1,24 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const N = Number(input[0]);
 const A = input[1].split(" ").map(Number);
-const lis = new Array(N);
+const lis = [];
 const binarySearch = (left, right, target) => {
     while(left < right){
-        const mid = Math.floor((left + right) / 2);
-
-        if(lis[mid] < target){
-            left = mid + 1;
-        } else{
-            right = mid;
-        }
+        const mid = (left + right) >> 1;
+        lis[mid] < target ? left = mid + 1 : right = mid;
     }
     return right;
 }
 
-let len = 0;
-lis[0] = A[0];
+lis.push(A[0]);
 
 for(let i = 1; i < N; i++){
-    if(lis[len] < A[i]){
-        lis[++len] = A[i];
+    if(lis[lis.length - 1] < A[i]){
+        lis.push(A[i]);
     } else{
-        const idx = binarySearch(0, len, A[i]);
+        const idx = binarySearch(0, lis.length - 1, A[i]);
         lis[idx] = A[i];
     }
 }
 
-console.log(len + 1);
+console.log(lis.length);
