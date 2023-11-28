42311f70ea19800785d99b216ce2bbc9c71235af
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-11-28 22:51:29 +0900

    [Gold V] Title: 두 용액, Time: 236 ms, Memory: 22408 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.js
+++ 백준/Gold/2470. 두 용액/두 용액.js
@@ -4,12 +4,13 @@ const solution = input[1].split(" ").map(Number).sort((a, b) => a - b);
 let left = 0;
 let right = N - 1;
 let min = Infinity;
-let result;
+let result = [0, 0];
 while(left < right){
     let sum = solution[left] + solution[right];
     if(Math.abs(sum) < min){
         min = Math.abs(sum);
-        result = [solution[left], solution[right]];
+        result[0] = solution[left];
+        result[1] = solution[right];
     }
     if(sum < 0){
         left++;
