7981ed21ed8e99da71f95637b361f071b1f70158
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-12-05 21:21:50 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 636 ms, Memory: 135400 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.js
@@ -0,0 +1,25 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const N = Number(input[0]);
+const A = input[1].split(" ").map(Number);
+const lis = [];
+
+const binarySearch = (left, right, target) => {
+    while(left < right){
+        const mid = (left + right) >> 1;
+        lis[mid] < target ? left = mid + 1 : right = mid;
+    }
+    return right;
+}
+
+lis.push(A[0]);
+
+for(let i = 0; i < N; i++){
+    if(lis[lis.length - 1] < A[i]){
+        lis.push(A[i]);
+    } else{
+        const idx = binarySearch(0, lis.length - 1, A[i]);
+        lis[idx] = A[i];
+    }
+}
+
+console.log(lis.length);
