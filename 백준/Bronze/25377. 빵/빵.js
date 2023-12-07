6ac20c6b5c97301604568fff8c2e7975b55341eb
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-12-08 06:48:46 +0900

    [Bronze IV] Title: 빵, Time: 132 ms, Memory: 9376 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.js
+++ 백준/Bronze/25377. 빵/빵.js
@@ -1,10 +1,8 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const N = Number(input[0]);
-let result = [0, Infinity];
+let min = Infinity;
 for(let i = 1; i <= N; i++){
     const [A, B] = input[i].split(" ").map(Number);
-    if(result[1] - result[0] > B - A){
-        result = [A, B];
-    } 
+    if(B >= A) min = Math.min(min, B);
 }
-console.log(result[1]);
+console.log(min === Infinity ? - 1 : min);
