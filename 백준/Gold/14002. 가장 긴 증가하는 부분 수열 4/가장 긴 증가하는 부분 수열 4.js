121e5871ea7f7767c424668e95ffe2ae9adfc7ad
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-12-09 12:27:16 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 188 ms, Memory: 9936 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.js
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.js
@@ -3,20 +3,20 @@ const N = Number(input[0]);
 const A = input[1].split(" ").map(Number);
 const dp = new Array(N).fill(0);
 let result = [];
-for(let i = 0; i < N; i++){
-    let max = 0;
-    let maxIdx = -1;
+
+for(let i = 1; i < N; i++){
     for(let j = 0; j < i; j++){
-        if(A[i] > A[j] && dp[j] > max){
-            max = dp[j];
-            maxIdx = j;
+        if(A[i] > A[j]){
+            dp[i] = Math.max(dp[i], dp[j] + 1);
         }
     }
-    dp[i] = max + 1;
-    result[i] = maxIdx !== -1 ? [...result[maxIdx].slice(), (A[i])] : [A[i]];
 }
-
-const maxLen = Math.max(...dp);
-
-console.log(maxLen);
-console.log(result[dp.indexOf(maxLen)].join(" "));
+let max = Math.max(...dp);
+console.log(max + 1);
+for(let i = N - 1; i >= 0; i--){
+    if(dp[i] === max){
+        result.push(A[i]);
+        max--;
+    }
+}
+console.log(result.reverse().join(" "));
