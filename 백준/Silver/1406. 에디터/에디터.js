996c25d61b06546bb9a8b49c9a4be43d7d7a8504
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-12-14 22:57:57 +0900

    [Silver II] Title: 에디터, Time: 548 ms, Memory: 107132 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.js
@@ -0,0 +1,20 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const leftStack = input[0].trim().split("");
+const rightStack = [];
+const M = Number(input[1]);
+for(let i = 2; i < M + 2; i++){
+    const [cmd, c] = input[i].trim().split(" ");
+    if(cmd === "L"){
+        if(leftStack.length) rightStack.push(leftStack.pop());
+    }
+    if(cmd === "D"){
+        if(rightStack.length) leftStack.push(rightStack.pop());
+    }
+    if(cmd === "B"){
+        if(leftStack.length) leftStack.pop();
+    }
+    if(cmd === "P"){
+        leftStack.push(c);
+    }
+}
+console.log([...leftStack, ...rightStack.reverse()].join(""));
