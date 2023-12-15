360b9f167351ef0fff9a6cbc92fe13d67ec3cc6d
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-12-15 22:51:09 +0900

    [Gold IV] Title: 오큰수, Time: 836 ms, Memory: 192884 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.js
+++ 백준/Gold/17298. 오큰수/오큰수.js
@@ -2,11 +2,13 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('
 const N = Number(input[0]);
 const A = input[1].split(" ").map(Number);
 const stack = [];
-const result = new Array(N).fill(-1);
 for(let i = 0; i < N; i++){
     while(stack.length && A[stack[stack.length - 1]] < A[i]){
-        result[stack.pop()] = A[i];
+        A[stack.pop()] = A[i];
     }
     stack.push(i);
 }
-console.log(result.join(" "));
+while(stack.length){
+    A[stack.pop()] = -1;
+}
+console.log(A.join(" "));
