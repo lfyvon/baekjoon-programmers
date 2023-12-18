a63f7636872aa320614c8eea32bec6dd4cdc9a47
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-12-18 22:53:21 +0900

    [Silver V] Title: 3의 배수, Time: 188 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Silver/1769. 3의 배수/3의 배수.js
+++ 백준/Silver/1769. 3의 배수/3의 배수.js
@@ -1,8 +1,12 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim();
 let count = 0;
 while(input.length > 1){
-    input = input.split("").map(Number).reduce((a, b) => a + b, 0).toString();
+    let sum = 0;
+    for(let i = 0; i < input.length; i++){
+        sum += +input[i];
+    }
+    input = sum.toString();
     count++;
 }
 console.log(count);
-console.log(+input % 3 ? "NO" : "YES");
+console.log(input % 3 ? "NO" : "YES");
