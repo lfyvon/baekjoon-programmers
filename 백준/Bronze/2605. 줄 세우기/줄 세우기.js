9b4ab53809380dd471302714c88178f84d6cacaa
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-12-21 21:41:38 +0900

    [Bronze II] Title: 줄 세우기, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.js
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const n = Number(input[0]);
+const std = input[1].split(" ").map(Number);
+let line = [1];
+for(let i = 1; i < n; i++){
+    line.splice(std[i], 0, i + 1);
+}
+console.log(line.reverse().join(" "));
