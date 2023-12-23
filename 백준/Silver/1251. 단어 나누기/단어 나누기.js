ce4512847214cd592d9a69df287fd594821f9470
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-12-23 18:14:06 +0900

    [Silver V] Title: 단어 나누기, Time: 128 ms, Memory: 11232 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.js
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.js
@@ -3,7 +3,7 @@ const result = [];
 const len = word.length;
 for(let i = 1; i < len - 1; i++){
     for(let j = i + 1; j < len; j++){
-        result.push([...word.slice(0, i).reverse(), ...word.slice(i, j).reverse(), ...word.slice(j).reverse()].join(""))
+        result.push(word.slice(0, i).reverse().join("") + word.slice(i, j).reverse().join("") + word.slice(j).reverse().join(""));
     }
 }
 console.log(result.sort()[0]);
