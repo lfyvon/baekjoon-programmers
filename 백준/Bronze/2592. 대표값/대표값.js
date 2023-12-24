6426b18e8aaa13ad87e85a44e729504ea6e3c74a
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-12-24 20:57:43 +0900

    [Bronze II] Title: 대표값, Time: 120 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/2592. 대표값/대표값.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(Number);
+const map = new Map();
+let avg = input.reduce((a, b) => a + b, 0) / 10;
+for(let i = 0; i < 10; i++){
+    if(map.has(input[i])){
+        map.set(input[i], map.get(input[i]) + 1);
+    } else{
+        map.set(input[i], 1);
+    }
+}
+const result = [...map];
+console.log(avg);
+console.log(result.sort((a, b) => b[1] - a[1])[0][0]);
