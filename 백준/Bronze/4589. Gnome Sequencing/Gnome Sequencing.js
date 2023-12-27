bf6f44f821684d2101e0bdf85021087decd03fe8
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-12-27 23:39:01 +0900

    [Bronze IV] Title: Gnome Sequencing, Time: 116 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const N = Number(input[0]);
+const result = ["Gnomes:"];
+for(let i = 1; i <= N; i++){
+    const [a, b, c] = input[i].split(" ").map(Number);
+    if((a <= b && b <= c) || (a >= b && b >= c)) result.push("Ordered");
+    else result.push("Unordered");
+}
+console.log(result.join("\n"));
