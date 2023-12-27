8f626c1471ba5aafc71b8c5cf3134e226265ff0d
lfyvon <110438191+lfyvon@users.noreply.github.com>
2023-12-27 23:40:47 +0900

    [Bronze IV] Title: Gnome Sequencing, Time: 108 ms, Memory: 9312 KB -BaekjoonHub

--- 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.js
+++ 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.js
@@ -3,7 +3,7 @@ const N = Number(input[0]);
 const result = ["Gnomes:"];
 for(let i = 1; i <= N; i++){
     const [a, b, c] = input[i].split(" ").map(Number);
-    if((a <= b && b <= c) || (a >= b && b >= c)) result.push("Ordered");
+    if((a < b && b < c) || (a > b && b > c)) result.push("Ordered");
     else result.push("Unordered");
 }
 console.log(result.join("\n"));
