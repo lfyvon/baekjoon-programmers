64b44441bbe5cd36ef922022fb46310f6dd81675
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-01-13 16:25:19 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    for (int i = 0; i < T; i++) {
+        int A, B;
+        scanf("%d %d", &A, &B);
+        printf("%d\n", A + B);
+    }
+    return 0;
+}
