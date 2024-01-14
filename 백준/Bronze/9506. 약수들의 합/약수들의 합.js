5d4b2d3a7c3c7430d02fc71db8083bbc0f776833
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-01-14 13:25:18 +0900

    [Bronze I] Title: 약수들의 합, Time: 112 ms, Memory: 9612 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.js
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.js
@@ -3,15 +3,16 @@ let i = 0;
 const result = [];
 while(input[i] !== -1){
     const num = input[i];
-    const div = [];
-    let sum = 0;
-    for(let j = 1; j < num; j++){
+    const div = [1];
+    let sum = 1;
+    for(let j = 2; j < Math.sqrt(num); j++){
         if(num % j === 0){
             div.push(j);
-            sum += j;
+            div.push(num / j);
+            sum += j + num / j;
         } 
     }
-    result.push(num === sum ? `${num} = ${div.join(" + ")}` : `${num} is NOT perfect.`);
+    result.push(num === sum ? `${num} = ${div.sort((a, b) => a - b).join(" + ")}` : `${num} is NOT perfect.`);
     i++;
 }
 console.log(result.join("\n"));
