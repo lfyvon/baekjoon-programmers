47bc39e5f54261f41e0c113483c7096540bdb796
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-01-18 00:01:10 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2420. 사파리월드/사파리월드.c
+++ 백준/Bronze/2420. 사파리월드/사파리월드.c
@@ -2,7 +2,7 @@
 
 int main() {
     long long N, M;
-    scanf("%lld %lld", &N, &M);
-    printf("%lld", llabs(N - M));
+    scanf("%ld %ld", &N, &M);
+    printf("%ld", labs(N - M));
     return 0;
 }
