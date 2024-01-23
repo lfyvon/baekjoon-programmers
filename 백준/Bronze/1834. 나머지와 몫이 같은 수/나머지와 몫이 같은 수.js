bc8461b93e4e3d43c38918959ee052d0528513f3
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-01-23 23:20:23 +0900

    [Bronze I] Title: 나머지와 몫이 같은 수, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.js
+++ 백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.js
@@ -1,6 +1,2 @@
 const N = BigInt(require('fs').readFileSync('/dev/stdin').toString().trim());
-let result = 0n;
-for(let i = 1n; i < N; i++){
-    result += (N + 1n) * i;
-}
-console.log(result.toString());
+console.log((N * (N - 1n) * (N + 1n) / 2n).toString());
