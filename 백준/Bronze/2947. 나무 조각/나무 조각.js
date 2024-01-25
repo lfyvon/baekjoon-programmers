517222b767e730b817d78284263afdc70bb2c6f3
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-01-25 22:57:29 +0900

    [Bronze I] Title: 나무 조각, Time: 116 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/2947. 나무 조각/나무 조각.js
+++ 백준/Bronze/2947. 나무 조각/나무 조각.js
@@ -1,20 +1,16 @@
 const wood = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
-const number = [1, 2, 3, 4, 5];
 const result = [];
 while(true){
     let isSame = true;
-
-    for(let i = 0; i < 5; i++){
-        if(number[i] !== wood[i]) isSame = false;
-    }
-
-    if(isSame) break;
-
+    
     for(let i = 0; i < 4; i++){
         if(wood[i] > wood[i + 1]){
             [wood[i], wood[i + 1]] = [wood[i + 1], wood[i]];
             result.push(wood.join(" "));
+            isSame = false;
         }
     }
+    
+    if(isSame) break;
 }
 console.log(result.join("\n"));
