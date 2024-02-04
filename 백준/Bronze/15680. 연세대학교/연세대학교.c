8a76e5e14d5d4073e434808ec83dd3e10b66069d
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-02-04 21:41:03 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    if (N == 0)
+    {
+        printf("YONSEI");
+    }
+    else
+    {
+        printf("Leading the Way to the Future");
+    }
+
+    return 0;
+}
