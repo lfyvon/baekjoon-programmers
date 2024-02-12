23da07a846ff3acdc86180bb2633e9f2c346d2e4
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-02-12 19:42:21 +0900

    [Silver IV] Title: GCD 합, Time: 168 ms, Memory: 9732 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.js
+++ 백준/Silver/9613. GCD 합/GCD 합.js
@@ -2,24 +2,14 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('
 const T = Number(input[0]);
 const result = [];
 
-let gcd = (a, b) => {
-    while(b > 0){
-      let r = a % b;
-      a = b;
-      b = r;
-    }
-    return a;
-  }
+const gcd = (num1, num2) => (num2 > 0 ? gcd(num2, num1 % num2) : num1);
 
 for(let i = 1; i <= T; i++){
     let [n, ...arr] = input[i].split(" ").map(Number);
-    arr = arr.sort((a, b) => b - a);
     let sum = 0;
     for(let j = 0; j < n - 1; j++){
-        const a = arr[j];
         for(let k = j + 1; k < n; k++){
-            const b = arr[k];
-            sum += gcd(a, b);
+            sum += gcd(arr[j], arr[k]);
         }
     }
     result.push(sum);
