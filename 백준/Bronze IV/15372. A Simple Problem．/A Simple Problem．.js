f7a5aebbd96adc70536f3c6687365ba18aad705b
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-02-17 07:55:36 +0900

    [Bronze IV] Title: A Simple Problem., Time: 384 ms, Memory: 65052 KB -BaekjoonHub

--- 백준/Bronze IV/15372. A Simple Problem．/A Simple Problem．.js
+++ 백준/Bronze IV/15372. A Simple Problem．/A Simple Problem．.js
@@ -1,6 +1,6 @@
-const [T, ...N] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(BigInt);
+const [T, ...N] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
 const result = [];
-for(let i = 0n; i < T; i++){
-    result.push((N[i] * N[i]).toString());
+for(let i = 0; i < T; i++){
+    result.push(N[i] ** 2);
 }
 console.log(result.join("\n"));
