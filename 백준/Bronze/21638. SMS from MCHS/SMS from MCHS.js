99bf8e836f869dcd7d6c1619a470f769564ee187
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-03-15 21:47:38 +0900

    [Bronze IV] Title: SMS from MCHS, Time: 128 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/21638. SMS from MCHS/SMS from MCHS.js
+++ 백준/Bronze/21638. SMS from MCHS/SMS from MCHS.js
@@ -1,7 +1,8 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const [t1, v1] = input[0].split(" ").map(Number);
 const [t2, v2] = input[1].split(" ").map(Number);
-if(t2 < 0 && v2 >= 10) console.log("A storm warning for tomorrow! Be careful and stay home if possible!");
-else if(t1 > t2) console.log("MCHS warns! Low temperature is expected tomorrow.");
-else if(t1 <= t2 && v2 > v1) console.log("MCHS warns! Strong wind is expected tomorrow.");
-else console.log("No message");
+let result = "No message";
+if(t2 < 0 && v2 >= 10) result = "A storm warning for tomorrow! Be careful and stay home if possible!";
+else if(t1 > t2) result = "MCHS warns! Low temperature is expected tomorrow.";
+else if(t1 <= t2 && v2 > v1) result = "MCHS warns! Strong wind is expected tomorrow.";
+console.log(result);
