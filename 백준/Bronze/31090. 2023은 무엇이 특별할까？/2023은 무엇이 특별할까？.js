0053b34e8e60944cbc53b2c28c9a1d7524dde62d
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-03-20 20:13:09 +0900

    [Bronze IV] Title: 2023은 무엇이 특별할까?, Time: 128 ms, Memory: 11132 KB -BaekjoonHub

--- 백준/Bronze/31090. 2023은 무엇이 특별할까？/2023은 무엇이 특별할까？.js
+++ 백준/Bronze/31090. 2023은 무엇이 특별할까？/2023은 무엇이 특별할까？.js
@@ -1,6 +1,6 @@
 const [T, ...N] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
 const result = [];
 for(let i = 0; i < T; i++){
-    result.push((N[i] + 1) % N[i].toString().slice(-2) === 0 ? "Good" : "Bye");
+    result.push((N[i] + 1) % (N[i] % 100) === 0 ? "Good" : "Bye");
 }
 console.log(result.join("\n"));
