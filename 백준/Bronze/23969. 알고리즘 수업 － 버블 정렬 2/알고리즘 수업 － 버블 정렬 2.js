4a79c1c9d3fef3941225ddfb34e8532c1c435f07
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-04-04 22:08:33 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 2, Time: 504 ms, Memory: 12524 KB -BaekjoonHub

--- 백준/Bronze/23969. 알고리즘 수업 － 버블 정렬 2/알고리즘 수업 － 버블 정렬 2.js
+++ 백준/Bronze/23969. 알고리즘 수업 － 버블 정렬 2/알고리즘 수업 － 버블 정렬 2.js
@@ -1,14 +1,13 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const [N, K] = input[0].split(" ").map(Number);
 const A = input[1].split(" ").map(Number);
-let count = 1;
 const bubbleSort = (arr) => {
+    let count = 1;
     for(let i = N; i > 1; i--){
         for(let j = 0; j < i; j++){
             if(A[j] > A[j + 1]){
                 [A[j], A[j + 1]] = [A[j + 1], A[j]];
-                if(count === K) return arr.join(" ");
-                count++;
+                if(count++ === K) return arr.join(" ");
             }
         }
     }
