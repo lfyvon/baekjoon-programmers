8a1030b1ee279676d682a7518eab02692483ace6
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-04-05 06:13:36 +0900

    [Bronze IV] Title: 그게 무슨 코드니.., Time: 116 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/31495. 그게 무슨 코드니．．/그게 무슨 코드니．．.js
+++ 백준/Bronze/31495. 그게 무슨 코드니．．/그게 무슨 코드니．．.js
@@ -1,3 +1,3 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim();
 const len = input.length;
-console.log(input.length > 2 && input[0] === '"' && input[len - 1] === '"' ? input.slice(1, len - 1) : "CE");
+console.log(len > 2 && input[0] === '"' && input[len - 1] === '"' ? input.slice(1, len - 1) : "CE");
