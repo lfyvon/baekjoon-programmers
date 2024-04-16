ae8c85044a340f9a4c817c07f099eaccb8072169
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-04-16 21:52:33 +0900

    [Bronze IV] Title: Easy-to-Solve Expressions, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/25784. Easy－to－Solve Expressions/Easy－to－Solve Expressions.js
+++ 백준/Bronze/25784. Easy－to－Solve Expressions/Easy－to－Solve Expressions.js
@@ -1,4 +1,2 @@
 const [a, b, c] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number).sort((a, b) => a - b);
-if(a + b === c) console.log(1);
-else if(a * b === c) console.log(2);
-else console.log(3);
+console.log(a + b === c ? 1 : a * b === c ? 2 : 3);
