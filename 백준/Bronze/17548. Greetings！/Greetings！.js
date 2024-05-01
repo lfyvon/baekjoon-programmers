de2f2430864e80afa64093a47c0edad541c556b4
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-05-01 22:06:59 +0900

    [Bronze IV] Title: Greetings!, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/17548. Greetings！/Greetings！.js
+++ 백준/Bronze/17548. Greetings！/Greetings！.js
@@ -1,3 +1,3 @@
 const str = require('fs').readFileSync('/dev/stdin').toString().trim();
 const len = str.length;
-console.log(str[0] + str.slice(1, len - 1).repeat(2) + str[len - 1]);
+console.log(str[0] + "e".repeat((len - 2) * 2) + str[len - 1]);
