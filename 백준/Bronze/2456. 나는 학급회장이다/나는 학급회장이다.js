d51dc65fb584de962def506e5038ed456bac29d9
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-05-12 22:28:48 +0900

    [Bronze I] Title: 나는 학급회장이다, Time: 120 ms, Memory: 9644 KB -BaekjoonHub

--- 백준/Bronze/2456. 나는 학급회장이다/나는 학급회장이다.js
+++ 백준/Bronze/2456. 나는 학급회장이다/나는 학급회장이다.js
@@ -1,15 +1,16 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const N = Number(input[0]);
 const stdNum = 3;
-const std = Array.from(new Array(stdNum), () => new Array(2).fill(0));
+const sum = new Array(stdNum).fill(0);
+const sqrt = new Array(stdNum).fill(0);
 for(let i = 1; i <= N; i++){
     const score = input[i].split(" ").map(Number);
     for(let j = 0; j < stdNum; j++){
-        std[j][0] += score[j];
-        std[j][1] += score[j] ** 2;
+        sum[j] += score[j];
+        sqrt[j] += score[j] ** 2;
     }
 }
-const max = Math.max(...std.map(el => el[1]));
-const maxIdx = [...std.map(el => el[1])].indexOf(max);
-let count = std.filter(el => el[1] === max).length;
-console.log(`${count > 1 ? 0 : maxIdx + 1} ${std[maxIdx][0]}`);
+const max = Math.max(...sqrt);
+const maxIdx = [...sqrt].indexOf(max);
+let count = sqrt.filter(el => el === max).length;
+console.log(`${count > 1 ? 0 : maxIdx + 1} ${sum[maxIdx]}`);
