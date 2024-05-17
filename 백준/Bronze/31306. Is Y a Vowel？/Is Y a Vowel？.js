c8b2af51d7c538e61cf97f67ad31f25ebf68d686
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-05-17 23:57:39 +0900

    [Bronze IV] Title: Is Y a Vowel?, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/31306. Is Y a Vowel？/Is Y a Vowel？.js
+++ 백준/Bronze/31306. Is Y a Vowel？/Is Y a Vowel？.js
@@ -1,8 +1,9 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim();
 const vowel = 'aeiou';
 let count = 0;
-let yCount = input.split("y").length - 1;
-for(let i = 0; i < vowel.length; i++){
-    count += input.split(`${vowel[i]}`).length - 1;
+let yCount = 0;
+for(let i = 0; i < input.length; i++){
+    if(vowel.includes(input[i])) count++;
+    if(input[i].includes('y')) yCount++;
 }
 console.log(count, count + yCount);
