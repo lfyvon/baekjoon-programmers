5c8ba031c82a5c9cc13efc19bf5d5d3bd91c4779
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-06-14 01:26:47 +0900

    [Bronze I] Title: GCD, Time: 112 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Bronze/5344. GCD/GCD.js
+++ 백준/Bronze/5344. GCD/GCD.js
@@ -3,7 +3,7 @@ const N = Number(input[0]);
 const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
 const result = [];
 for(let i = 1; i <= N; i++){
-    const [A, B] = input[i].split(" ").map(Number).filter(el => el);
+    const [A, B] = input[i].split(" ").map(Number).sort((a, b) => (b - a));
     result.push(gcd(Math.max(A, B), Math.min(A, B)));
 }
 console.log(result.join("\n"));
