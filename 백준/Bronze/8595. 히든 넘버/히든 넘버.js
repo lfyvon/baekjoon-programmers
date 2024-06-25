0c0ebfcd35f8f20f9219ffe2de4b90b7d9773ed2
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-06-25 22:53:19 +0900

    [Bronze I] Title: 히든 넘버, Time: 408 ms, Memory: 24324 KB -BaekjoonHub

--- 백준/Bronze/8595. 히든 넘버/히든 넘버.js
+++ 백준/Bronze/8595. 히든 넘버/히든 넘버.js
@@ -2,14 +2,14 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('
 const n = Number(input[0]);
 const word = input[1];
 let num = '';
-let result = 0n;
+let result = 0;
 for(let i = 0; i < n; i++){
     if(word[i].charCodeAt() >= 45 && word[i].charCodeAt() <= 57){
         num += word[i];
     } else{
-        result += BigInt(num);
+        result += Number(num);
         num = '';
     }
 }
-result += BigInt(num);
+result += Number(num);
 console.log(result.toString());
