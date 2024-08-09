53b8d8ecb926eddc3b660d07416fc96b6841d028
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-08-09 23:28:38 +0900

    [Bronze II] Title: Box of Bricks, Time: 88 ms, Memory: 9612 KB -BaekjoonHub

--- 백준/Bronze/6318. Box of Bricks/Box of Bricks.js
+++ 백준/Bronze/6318. Box of Bricks/Box of Bricks.js
@@ -7,6 +7,6 @@ while(input[i] !== '0'){
     const avg = arr.reduce((a, b) => a + b, 0) / N;
     const sum = arr.reduce((a, b) => a + Math.abs(b - avg), 0) / 2;
     result.push(`Set #${i / 2}`);
-    result.push(`The minimum number of moves is ${sum}.` + '\n');
+    result.push(`The minimum number of moves is ${sum}.\n`);
 }
 console.log(result.join("\n"));
