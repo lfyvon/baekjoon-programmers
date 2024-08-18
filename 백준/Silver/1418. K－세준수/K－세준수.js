7a835fcc0f8527dbd1b54d3dc109e9e83af3c672
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-08-18 23:04:09 +0900

    [Silver V] Title: K-세준수, Time: 160 ms, Memory: 10544 KB -BaekjoonHub

---
+++ 백준/Silver/1418. K－세준수/K－세준수.js
@@ -0,0 +1,18 @@
+const [N, K] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+let primeList = Array(N + 1).fill(0);
+let result = 0;
+
+for(let i = 2; i <= N; i++){
+    if(!primeList[i]){
+        for(let j = i; j <= N; j += i){
+            if(j % i === 0){
+                primeList[j] = Math.max(primeList[j], i);
+            }
+        }
+    }
+
+}
+for(let i = 1; i <= N; i++){
+    if(primeList[i] <= K) result++;
+}
+console.log(result)
