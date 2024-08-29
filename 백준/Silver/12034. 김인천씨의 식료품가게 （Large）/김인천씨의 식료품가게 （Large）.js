66a05646aab5ef51186fb4b4e64f2dd48e49e6f4
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-08-29 23:56:14 +0900

    [Silver V] Title: 김인천씨의 식료품가게 (Large), Time: 184 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Silver/12034. 김인천씨의 식료품가게 （Large）/김인천씨의 식료품가게 （Large）.js
@@ -0,0 +1,24 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const T = Number(input[0]);
+let idx = 1;
+let count = 0;
+const result = [];
+
+while(count < T){
+    const N = input[idx++];
+    const P = input[idx++].split(" ").map(Number);
+    const arr = [];
+    for(let i = 0; i < N * 2; i++){
+        if(!P[i]) continue;
+        for(let j = i + 1; j < N * 2; j++){
+            if(P[i] === P[j] * 0.75){
+                arr.push(P[i]);
+                P[i] = 0;
+                P[j] = 0;
+            }
+        }
+    }
+    result.push(`Case #${count + 1}: ${arr.join(" ")}`);
+    count++;
+}
+console.log(result.join("\n"));
