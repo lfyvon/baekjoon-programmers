7c4d326a227945d9a2fbe1a14738e4c10da9b701
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-09-03 00:05:29 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 668 ms, Memory: 97076 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.js
@@ -0,0 +1,21 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const [N, M, R] = input[0].split(" ").map(Number);
+const visited = new Array(N + 1).fill(0);
+const graph = Array.from(Array(N + 1), () => []);
+let count = 1;
+for(let i = 1; i <= M; i++){
+    const [x, y] = input[i].split(" ").map(Number);
+    graph[x].push(y);
+    graph[y].push(x);
+}
+graph.map(el => el.sort((a, b) => b - a));
+const dfs = (R) => {
+    if(!visited[R]){
+        visited[R] = count++;
+        for(let x of graph[R]){
+            dfs(x);
+        }
+    }
+}
+dfs(R);
+console.log(visited.slice(1).join("\n"));
