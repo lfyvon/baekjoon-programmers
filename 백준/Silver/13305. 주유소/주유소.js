ce7062640d7d2f17298ba3c75e9fdd7a1e45be7a
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-10-26 00:28:10 +0900

    [Silver III] Title: 주유소, Time: 288 ms, Memory: 44456 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,13 +1,13 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const N = Number(input[0]);
-const dis = input[1].split(" ").map(Number);
-const price = input[2].split(" ").map(Number);
+const dis = input[1].split(" ").map(BigInt);
+const price = input[2].split(" ").map(BigInt);
 let idx = 0;
-let result = 0;
+let result = 0n;
 for(let i = 0; i < N - 1; i++){
     result += price[idx] * dis[i];
     if(price[idx] > price[i + 1]){
-        idx++;
+        idx = i + 1;
     }
 }
-console.log(result)
+console.log(result.toString())
