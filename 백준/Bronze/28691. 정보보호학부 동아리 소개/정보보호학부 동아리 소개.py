4f6747e95237ea0c6768b20ecebd389a0d914729
lfyvon <110438191+lfyvon@users.noreply.github.com>
2024-11-08 00:45:35 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+group = input()
+
+if group == "M":
+    print("MatKor")
+elif group == "W":
+    print("WiCys")
+elif group == "C":
+    print("CyKor")
+elif group == "A":
+    print("AlKor")
+else:
+    print("$clear")
