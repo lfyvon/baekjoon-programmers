dd1f8bd4f39688338affd9e3b30c3ef7430e3a69
lfyvon <110438191+lfyvon@users.noreply.github.com>
2025-03-30 23:22:39 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 88 ms, Memory: 10140 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const N = Number(fs.readFileSync(process.platform === "linux" ? 0 : './test.txt', "utf-8").toString().trim());
+const result = [];
+
+for(let i = 0; i < N * 2; i++){
+    result.push(`${"@".repeat(N)}${" ".repeat(N * 3)}${"@".repeat(N)}`);
+}
+
+for(let i = 0; i < N; i++){
+    result.push("@@@@@".repeat(N));
+}
+
+for(let i = 0; i < N; i++){
+    result.push(`${"@".repeat(N)}${" ".repeat(N * 3)}${"@".repeat(N)}`);
+}
+
+for(let i = 0; i < N; i++){
+    result.push("@@@@@".repeat(N));
+}
+
+console.log(result.join("\n"));
