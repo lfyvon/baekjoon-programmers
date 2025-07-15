fbca7131d4ed9e8f684f4c36457bee525487502f
lfyvon <110438191+lfyvon@users.noreply.github.com>
2025-07-16 00:07:18 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const input = fs.readFileSync(process.platform === "linux" ? 0 : './test.txt', "utf-8").toString().trim().split("\n");
+const N = Number(input[0]);
+console.log('yes\n'.repeat(N).trim());
