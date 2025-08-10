a126580ef54345cd26bcf5d4b164853ff3610584
lfyvon <110438191+lfyvon@users.noreply.github.com>
2025-08-10 23:16:03 +0900

    [Silver V] Title: 점프는 즐거워, Time: 104 ms, Memory: 11352 KB -BaekjoonHub

---
+++ 백준/Silver/4383. 점프는 즐거워/점프는 즐거워.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync(process.platform === "linux" ? 0 : './test.txt', "utf-8").toString().trim().split("\n");
+const result = [];
+let idx = 0;
+
+while(input[idx]){
+    const [n, ...arr] = input[idx++].trim().split(" ").filter(el => el).map(Number);
+    const num = new Array(n).fill(0);
+    num[0] = 1;
+    for(let i = 1; i < n; i++){
+        let diff = Math.abs(arr[i] - arr[i - 1]);
+        if(diff > 0 && diff < n){
+            num[diff] = 1;
+        } 
+    }
+    if(num.filter(el => el === 1).length === n){
+        result.push("Jolly");
+    } else{
+        result.push("Not jolly");        
+    }
+}
+console.log(result.join("\n"));
