16dd42c74bab17afe127d79fe391a203c2d3a16b
lfyvon <110438191+lfyvon@users.noreply.github.com>
2025-09-18 00:22:31 +0900

    [Bronze III] Title: 단원평가, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/31798. 단원평가/단원평가.js
+++ 백준/Bronze/31798. 단원평가/단원평가.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const input = fs.readFileSync(process.platform === "linux" ? 0 : './test.txt', "utf-8").toString().trim().split("\n");
 const [a, b, c] = input[0].split(" ").map(Number);
-console.log(c === 0 ? Math.pow(a + b, 0.5) : c ** 2 - Math.max(a, b));
+console.log(c === 0 ? Math.sqrt(a + b) : c ** 2 - Math.max(a, b));
